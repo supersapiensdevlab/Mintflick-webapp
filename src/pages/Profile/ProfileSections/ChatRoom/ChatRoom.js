bc155457e538b935c6b469b91a2e2e829c5de758
Prasad Karmalkar <prasadkarmalkar2020@gmail.com>
2022-01-27 15:08:28 +0530

    Bug fix

--- src/pages/Profile/ProfileSections/ChatRoom/ChatRoom.js
+++ src/pages/Profile/ProfileSections/ChatRoom/ChatRoom.js
@@ -1,53 +1,52 @@
-
 import React, { useEffect, useReducer, useRef, useState } from 'react';
 import './chatroom.css';
 import io from 'socket.io-client';
+import moment from 'moment';
 function ChatRoom(props) {
   // to get loggedin user from   localstorage
   const user = JSON.parse(window.localStorage.getItem('user'));
-
 
   const chatRef = useRef(null);
   // the form state manages the form input for creating a new message
   const [formState, setForm] = useState({
     message: '',
   });
-  const [messages,setMessages] = useState([])
-  const [currentSocket, setCurrentSocket] = useState(null)
-
+  const [messages, setMessages] = useState([]);
+  const [currentSocket, setCurrentSocket] = useState(null);
+let currentDate = null;
   useEffect(() => {
     // initialize gun locally
     if (user) {
       const socket = io(process.env.REACT_APP_SERVER_URL);
-      setCurrentSocket(socket)
-      socket.emit('joinroom',{user_id:user._id,room_id:props.userp._id})
-      socket.on('init',(msgs)=>{
+      setCurrentSocket(socket);
+      socket.emit('joinroom', { user_id: user._id, room_id: props.userp._id });
+      socket.on('init', (msgs) => {
         setMessages(msgs);
         chatRef.current.scrollIntoView({ behavior: 'smooth' });
-      })
-      socket.on('message2',(msg)=>{
-        setMessages(prevArray => [...prevArray, msg])
+      });
+      socket.on('message2', (msg) => {
+        setMessages((prevArray) => [...prevArray, msg]);
         chatRef.current.scrollIntoView({ behavior: 'smooth' });
-      })
+      });
     } else {
       window.history.replaceState({}, 'Home', '/');
     }
     // eslint-disable-next-line
-    return () => { socket.disconnect() }
+
   }, []);
 
   // set a new message in gun, update the local state to reset the form field
   function saveMessage(e) {
     e.preventDefault();
     let chat = {
-      room_id:props.userp._id,
-      user_id:user._id,
-      username:user.username,
-      profile_image:user.profile_image,
-      type:'text',
-      message:formState.message
-    }
-    currentSocket.emit('chatMessage',chat)
+      room_id: props.userp._id,
+      user_id: user._id,
+      username: user.username,
+      profile_image: user.profile_image,
+      type: 'text',
+      message: formState.message,
+    };
+    currentSocket.emit('chatMessage', chat);
     setForm({
       message: '',
     });
@@ -63,37 +62,60 @@ function ChatRoom(props) {
       <div className="overflow-hidden">
         <main className="chat-container-height">
           <div className="p-2 chat-height overflow-y-scroll	">
-            {messages.map((message) => (
-              <div
-                className="px-6 p-2 flex items-center	rounded-xl dark: bg-dbeats-dark-secondary	mb-2"
-                key={message.createdAt}
-              >
-                <div className="chat_message_profile">
-                  <img
-                    height="50px"
-                    width="50px"
-                    className="rounded-full"
-                    alt="profile"
-                    src={message.profile_image}
-                  />
-                </div>
-                <div className="p-1">
-                  <p
-                    className={
-                      message.username === user.username
-                        ? 'text-base font-bold mb-1  text-blue-400'
-                        : 'text-base font-bold mb-1 text-white	'
+            {messages
+              ? messages.map((message) => {
+                  const d = new Date(message.createdAt.toString());
+                  let od ;
+                  let dis = null;
+                  if (currentDate == null) {
+                    currentDate = d.toDateString()
+                    dis = d.toDateString();
+                  }else{
+                   od  = new Date(currentDate);
+                    if(d>od){
+                      dis = d.toDateString();
+                      currentDate = d.toDateString()
+                    }else{
+                      dis = null;
                     }
-                  >
-                    {message.username}{' '}
-                    <span className="text-sm text-gray-300 font-light">
-                      {new Date(message.createdAt).toDateString()}
-                    </span>
-                  </p>
-                  <p className="text">{message.message}</p>
-                </div>
-              </div>
-            ))}
+                  }
+
+                  return (
+                    <>
+                    {dis}
+                      <div
+                        className="px-6 p-2 flex items-center	rounded-xl dark: bg-dbeats-dark-secondary	mb-2"
+                        key={message.createdAt}
+                      >
+                        <div className="chat_message_profile">
+                          <img
+                            height="50px"
+                            width="50px"
+                            className="rounded-full"
+                            alt="profile"
+                            src={message.profile_image}
+                          />
+                        </div>
+                        <div className="p-1">
+                          <p
+                            className={
+                              message.username === user.username
+                                ? 'text-base font-bold mb-1  text-blue-400'
+                                : 'text-base font-bold mb-1 text-white	'
+                            }
+                          >
+                            {message.username}{' '}
+                            <span className="text-sm text-gray-300 font-light">
+                              {new Date(message.createdAt).toDateString()}
+                            </span>
+                          </p>
+                          <p className="text">{message.message}</p>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })
+              : '<></>'}
             <div ref={chatRef} />
           </div>
         </main>
