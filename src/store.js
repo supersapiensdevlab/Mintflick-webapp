344c6d16868c6545cf32319b53b7cbb0d1b6014e
Rishikeshk9 <rushikeshkardile9@gmail.com>
2022-03-19 19:48:31 +0530

    Fixed React Dev tools extension

--- src/store.js
+++ src/store.js
@@ -1,13 +1,22 @@
-import { createStore ,applyMiddleware , compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-
-const middleWare = [ thunk ];
+const middleWare = [thunk];
 const initialState = {};
 
-
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleWare,), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-
+let store = null;
+if (process.env.NODE_ENV === 'development') {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleWare),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),
+  );
+} else {
+  store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleWare)));
+}
 
 export default store;
