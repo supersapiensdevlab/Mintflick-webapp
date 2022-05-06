d59ef2dc1110a2dc2bb1df3cff220ee53f645a2e
Sahil412000 <sahilpanjabi123@gmail.com>
2022-05-06 19:18:02 +0530

    Fix Pinned Panel

--- src/component/Pinned_Panel/Pinned_Panel.js
+++ src/component/Pinned_Panel/Pinned_Panel.js
@@ -5,79 +5,75 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import personImg from '../../assets/images/profile.svg';
 import useWeb3Modal from '../../hooks/useWeb3Modal';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 const PinnedPanel = () => {
   const darkMode = useSelector((state) => state.toggleDarkMode);
-  const userdata = useSelector((state) => state.User.user); 
+  const userdata = useSelector((state) => state.User.user);
   const history = useHistory();
   const [loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
   const [pinnedData, setPinnedData] = useState([]);
 
-  const getPinnedData = async (data) => {
-    for (let i = 0; i < data.length; i++) {
-      await axios.get(`${process.env.REACT_APP_SERVER_URL}/user/${data[i]}`).then((value) => {
-        setPinnedData((oldArray) => [...oldArray, value.data]);
-      });
-    }
-  };
-
   useEffect(() => {
-    let data = JSON.parse(window.localStorage.getItem('pinned_user'));
-    if (data && data.length > 0) {
-      getPinnedData(data);
-    } else {
-      if (userdata) {
-        if (userdata.pinned) {
-          window.localStorage.setItem('pinned_user', JSON.stringify(userdata.pinned));
-          getPinnedData(userdata.pinned);
-        }
+    if (userdata && userdata.pinned) {
+      for (let i = 0; i < userdata.pinned.length; i++) {
+        axios
+          .get(`${process.env.REACT_APP_SERVER_URL}/user/${userdata.pinned[i]}`)
+          .then((value) => {
+            setPinnedData((oldArray) => [...oldArray, value.data]);
+          });
       }
     }
     // eslint-disable-next-line
-  }, []);
+  }, [userdata]);
 
-  const handlePlusClick = async() =>{
-    if(userdata){
+  const handlePlusClick = async () => {
+    if (userdata) {
       history.push(`/profile/${userdata.username}/following`);
-    }else{
+    } else {
       await loadWeb3Modal();
     }
-  }
+  };
   return (
     <div className={` w-full fixed top-0 ${darkMode && 'dark'} z-2  `}>
       <div
         className={`hidden   lg:block 2xl:pt-16 lg:pt-14 bg-white w-max shadow  z-10 h-full fixed  nm-flat-dbeats-dark-secondary  px-2  dark:text-gray-100  flex flex-col justify-center `}
       >
         {/* Subscribed User Avatar */}
-        {pinnedData.map((pinnedUser, i) => {
-          return (
-            <div key={i} className="flex justify-center content-center w-full cursor-pointer ">
-              <OverlayTrigger
-                placement="bottom"
-                overlay={
-                  <Tooltip
-                    id="button-tooltip-2"
-                    className="bg-blue-50 shadow-md dark:bg-dbeats-dark-primary   z-20 ml-16 -mt-10 mb-1 px-3 py-1 rounded-lg"
+        {userdata && pinnedData ? (
+          <>
+            {pinnedData.map((pinnedUser, i) => {
+              console.log(pinnedUser.username);
+              return (
+                <div key={i} className="flex justify-center content-center w-full cursor-pointer ">
+                  <OverlayTrigger
+                    placement="bottom"
+                    overlay={
+                      <Tooltip
+                        id="button-tooltip-2"
+                        className="bg-blue-50 shadow-md dark:bg-dbeats-dark-primary   z-20 ml-16 -mt-10 mb-1 px-3 py-1 rounded-lg"
+                      >
+                        <span className="text-md">{pinnedUser.username}</span>
+                      </Tooltip>
+                    }
                   >
-                    <span className="text-md">{pinnedUser.username}</span>
-                  </Tooltip>
-                }
-              >
-                <Link to={`/profile/${pinnedUser.username}/`} className=" my-2 relative">
-                  <div className="p-0.5 nm-flat-dbeats-dark-secondary rounded-full hover:nm-inset-dbeats-dark-secondary">
-                    <img
-                      src={pinnedUser.profile_image ? pinnedUser.profile_image : personImg}
-                      alt=""
-                      className=" 2xl:w-14 2xl:h-14 lg:h-10 lg:w-10 rounded-full hover:shadow hover:scale-99 transform transition-all"
-                    />{' '}
-                  </div>
-                </Link>
-              </OverlayTrigger>
-            </div>
-          );
-        })}
-
+                    <Link to={`/profile/${pinnedUser.username}/`} className=" my-2 relative">
+                      <div className="p-0.5 nm-flat-dbeats-dark-secondary rounded-full hover:nm-inset-dbeats-dark-secondary">
+                        <img
+                          src={pinnedUser.profile_image ? pinnedUser.profile_image : personImg}
+                          alt=""
+                          className=" 2xl:w-14 2xl:h-14 lg:h-10 lg:w-10 rounded-full hover:shadow hover:scale-99 transform transition-all"
+                        />{' '}
+                      </div>
+                    </Link>
+                  </OverlayTrigger>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <></>
+        )}
         <div className="flex justify-center cursor-pointer  ">
           <a
             onClick={handlePlusClick}
