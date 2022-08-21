a9a3fc46463bf81d721d544eb7e81d419168c755
Rushikesh Kardile <rushikeshkardile9@gmail.com>
2022-08-21 19:21:33 +0530

    Fixed New user Creation

--- src/Componants/Wallet/CreateNewUser.jsx
+++ src/Componants/Wallet/CreateNewUser.jsx
@@ -17,7 +17,7 @@ function CreateNewUser() {
     console.log(walletAddress);
     await axios({
       method: "post",
-      url: `${process.env.REACT_APP_API_BASE_URL}/user/getuser_by_wallet`,
+      url: `${process.env.REACT_APP_SERVER_URL}/user/getuser_by_wallet`,
       data: {
         walletId: walletAddress,
         email: email,
@@ -39,10 +39,10 @@ function CreateNewUser() {
       });
   }
   return (
-    <div className="flex flex-col justify-start w-fit lg:w-1/2 space-y-6 p-6 lg:p-12">
-      <p className="text-5xl font-bold text-brand-gradient">Create Account</p>
-      <div className="text-brand4 text-lg font-medium">
-        <p className="text-xl font-semibold text-brand2">
+    <div className='flex flex-col justify-start w-fit lg:w-1/2 space-y-6 p-6 lg:p-12'>
+      <p className='text-5xl font-bold text-brand-gradient'>Create Account</p>
+      <div className='text-brand4 text-lg font-medium'>
+        <p className='text-xl font-semibold text-brand2'>
           Just enter your email!
         </p>
         MintFlick will send important notifications to this email.
@@ -55,18 +55,16 @@ function CreateNewUser() {
             ? createNewUser(State.database.walletAddress)
             : seterror("Enter name and email!");
         }}
-        className="w-full max-w-lg flex flex-col  space-y-4   pt-4"
-      >
+        className='w-full max-w-lg flex flex-col  space-y-4   pt-4'>
         {error ? (
-          <div className="flex justify-between items-center p-3 bg-rose-600 text-slate-100 rounded-lg font-mediumtext-sm">
-            <div className="flex items-center gap-4">
+          <div className='flex justify-between items-center p-3 bg-rose-600 text-slate-100 rounded-lg font-mediumtext-sm'>
+            <div className='flex items-center gap-4'>
               <AlertTriangle></AlertTriangle> {error}
             </div>
             <X
-              className="cursor-pointer"
+              className='cursor-pointer'
               onClick={() => seterror("")}
-              size={16}
-            ></X>
+              size={16}></X>
           </div>
         ) : (
           <></>
@@ -74,19 +72,18 @@ function CreateNewUser() {
         <input
           value={name}
           onChange={(e) => setname(e.target.value)}
-          placeholder="Name"
-          className="input w-full"
+          placeholder='Name'
+          className='input w-full'
         />
         <input
           value={email}
           onChange={(e) => setemail(e.target.value)}
-          placeholder="Email"
-          className="input w-full "
+          placeholder='Email'
+          className='input w-full '
         />
         <button
-          type="submit"
-          className={`btn  w-full ${loader ? "loading" : "btn-brand"}`}
-        >
+          type='submit'
+          className={`btn  w-full ${loader ? "loading" : "btn-brand"}`}>
           Create Account
         </button>
       </form>
