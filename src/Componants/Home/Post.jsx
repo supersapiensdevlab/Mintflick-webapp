2029be47b9ea12f54d92ada4c45a86f75648fb64
Sahil <sahil@Deepaks-MacBook-Pro.local>
2022-09-27 12:03:38 +0530

    ui updated

--- src/Componants/Home/Post.jsx
+++ src/Componants/Home/Post.jsx
@@ -700,7 +700,7 @@ function Post(props) {
       <div className="relative w-full h-fit lg:bg-slate-100 lg:dark:bg-slate-800 lg:rounded-xl p-4 lg:p-8 space-y-4 pb-4 border-b-2 lg:border-none  border-slate-200 dark:border-slate-900">
         {props.tokenId && (
           <svg
-            className="absolute -top-6 lg:top-0 left-4 "
+            className="absolute -top-6 lg:-top-1 left-4 "
             width="30"
             height="36"
             viewBox="0 0 30 41"
