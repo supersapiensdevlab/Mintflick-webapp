d3ebfb01c03d7329ea3ad7df031ba8da6d74b603
Rushikesh Kardile <rushikeshkardile9@gmail.com>
2022-11-08 01:23:40 +0530

    Added Vertical padding to Live Section & Events Page

--- src/Componants/Live/Live.jsx
+++ src/Componants/Live/Live.jsx
@@ -7,18 +7,17 @@ import ScheduledStream from "./ScheduledStream";
 
 function Live() {
   return (
-    <div className=" flex w-full h-screen  bg-white dark:bg-slate-900 ">
-      <div className="hidden lg:flex flex-col h-full w-1/4 ml-12 pt-24  space-y-6 overflow-y-auto">
+    <div className=' flex w-full h-screen  bg-white dark:bg-slate-900  '>
+      <div className='hidden lg:flex flex-col h-full w-1/4 ml-12 pt-24  space-y-6 overflow-y-auto'>
         <LiveChannels></LiveChannels>
       </div>
-      <div className="w-full lg:w-3/4 lg:mr-12 h-full pt-16 lg:pt-24  space-y-6 overflow-y-auto">
+      <div className='w-full lg:w-3/4 lg:mr-12 h-full pt-16 lg:pt-24  space-y-6 overflow-y-auto pb-12'>
         <LiveBanners></LiveBanners>
         <LiveChannelCategories
-          section_name="Live Now"
-          event_status="Live"
-        ></LiveChannelCategories>
+          section_name='Live Now'
+          event_status='Live'></LiveChannelCategories>
         <LiveCategories></LiveCategories>
-        <ScheduledStream section_name="Scheduled Streams"></ScheduledStream>
+        <ScheduledStream section_name='Scheduled Streams'></ScheduledStream>
       </div>
     </div>
   );
