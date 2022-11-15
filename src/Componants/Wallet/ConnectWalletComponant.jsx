8c5358bfb5d0af9cbef625b216b99d96c7e4cc12
Sahil <sahil@Deepaks-MacBook-Pro.local>
2022-11-15 22:01:06 +0530

    login changes

--- src/Componants/Wallet/ConnectWalletComponant.jsx
+++ src/Componants/Wallet/ConnectWalletComponant.jsx
@@ -7,6 +7,8 @@ import { UserContext } from "../../Store";
 import PolygonToken from "../../Assets/logos/PolygonToken";
 import SolanaToken from "../../Assets/logos/SolanaToken";
 import useWeb3Auth from "../../Hooks/useWeb3Auth";
+import { Rocket } from "tabler-icons-react";
+import Emoji from "react-emojis";
 
 function ConnectWalletComponant() {
   const wallets = [twitter, discord, instagram];
@@ -22,50 +24,53 @@ function ConnectWalletComponant() {
 
   return (
     <div
-      className='flex flex-col justify-start w-fit lg:w-1/2 space-y-6 p-6
-      lg:p-12 bg-texture bg-repeat'>
-      <p className='text-5xl font-bold text-brand-gradient'>Connect Wallet</p>
-      <p className='text-brand4 text-lg font-medium'>
-        <p className='text-xl font-semibold text-brand2'>What is wallet?</p>
+      className="flex flex-col justify-start w-fit lg:w-1/2 space-y-6 p-6
+      lg:p-12 bg-texture bg-repeat"
+    >
+      <p className="text-5xl font-bold text-brand-gradient">Connect Wallet</p>
+      <p className="text-brand4 text-lg font-medium">
+        <p className="text-xl font-semibold text-brand2">What is wallet?</p>
         Crypto wallets store your private keys, keeping your crypto safe and
         accessible. They also allow you to send, receive, and spend
         cryptocurrencies like Bitcoin and Ethereum.
       </p>
-      <p className='text-brand4 text-lg font-medium'>
+      <p className="text-brand4 text-lg font-medium">
         Connect your crypto wallet to mint your NFT's in Mintflick.If you do not
         have a wallet then just create one.
       </p>
-      <div className='w-full flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 pt-10'>
+      <div className="w-full flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4 pt-10">
         <button
           onClick={async () => {
             await login();
           }}
-          className='btn btn-brand sm:w-1/2'>
-          Connect wallet
+          className="btn btn-brand w-full capitalize gap-1"
+        >
+          <Emoji emoji="rocket" />
+          Get Started
         </button>
         {/* <button onClick={getUserInfo} className="card">
           Get User Info
         </button> */}
-        <button
+        {/* <button
           onClick={async () => {
             login();
           }}
           className='btn btn-outline btn-primary sm:w-1/2'>
           Create new wallet
-        </button>
+        </button> */}
       </div>
-      <div className='flex text-white align-middle text-center gap-x-2'>
+      <div className="flex text-white align-middle text-center gap-x-2">
         Supports <SolanaToken size={24}></SolanaToken>
       </div>
-      <div className='form-control w-fit  hidden'>
-        <label className='label cursor-pointer gap-4'>
-          <span className='label-text text-white'>Switch Chains</span>
+      <div className="form-control w-fit  hidden">
+        <label className="label cursor-pointer gap-4">
+          <span className="label-text text-white">Switch Chains</span>
           <SolanaToken
             className={State.database.chainId === 1 ? "saturate-0" : null}
           />
           <input
-            type='checkbox'
-            className='toggle bg-brand '
+            type="checkbox"
+            className="toggle bg-brand "
             checked={State.database.chainId === 0 ? false : true}
             onChange={() => {
               let _selectedChain = State.database.chainId === 0 ? 1 : 0;
@@ -82,18 +87,18 @@ function ConnectWalletComponant() {
           ) : null} */}
         </label>
       </div>
-      <div className='w-full md:w-fit border p-4 space-y-2 rounded-lg border-slate-800'>
-        <p className='text-lg font-medium text-brand4'>
+      <div className="w-full md:w-fit border p-4 space-y-2 rounded-lg border-slate-800">
+        <p className="text-lg font-medium text-brand4">
           Join the MintFlick community
         </p>
-        <div className='w-full flex flex-wrap justify-between md:space-x-2'>
+        <div className="w-full flex flex-wrap justify-between md:space-x-2">
           {wallets.map((wallet, i) => (
-            <a href={links[i]} target='_blank' rel='noreferrer'>
+            <a href={links[i]} target="_blank" rel="noreferrer">
               <img
                 key={i}
-                className='h-16 w-16 bg-slate-800 rounded-full p-3 text-white'
+                className="h-16 w-16 bg-slate-800 rounded-full p-3 text-white"
                 src={wallet}
-                alt='wallet'
+                alt="wallet"
               />
             </a>
           ))}
