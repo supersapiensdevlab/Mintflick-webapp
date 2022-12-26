98a01442bdbd8e0f29ee0ec95e81cffb78f1ac7d
Rushikesh Kardile <supersapiens@Orions-AirBook.local>
2022-12-27 04:55:44 +0530

    Event Creation linked to Database

--- src/Componants/Event/CreateEvent.jsx
+++ src/Componants/Event/CreateEvent.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import moment from "moment";
 import { ethers } from "ethers";
+import axios from "axios";
 
 import {
   AlertTriangle,
@@ -33,22 +34,354 @@ function CreateEvent() {
   const [endDate, setendDate] = useState("");
   const [timezone, settimezone] = useState("");
   const timezones = [
-    "Europe/Andorra",
-    "Asia/Dubai",
-    "Asia/Kabul",
-    "Europe/Tirane",
-    "Asia/Yerevan",
-    "America/Argentina/Jujuy",
-    "America/Fortaleza",
-    "Europe/Berlin",
-    "Europe/Copenhagen",
-    "America/Santo_Domingo",
-    "Africa/Algiers",
-    "America/Guayaquil",
-    "Pacific/Nauru",
-    "Pacific/Funafuti",
-    "Asia/Taipei",
-    "Europe/Kiev",
+    'Africa/Abidjan',
+    'Africa/Accra',
+    'Africa/Algiers',
+    'Africa/Bissau',
+    'Africa/Cairo',
+    'Africa/Casablanca',
+    'Africa/Ceuta',
+    'Africa/El_Aaiun',
+    'Africa/Johannesburg', 
+    'Africa/Juba',
+    'Africa/Khartoum',
+    'Africa/Lagos',
+    'Africa/Maputo',
+    'Africa/Monrovia',
+    'Africa/Nairobi',
+    'Africa/Ndjamena',
+    'Africa/Sao_Tome',
+    'Africa/Tripoli',
+    'Africa/Tunis',
+    'Africa/Windhoek',
+    'America/Adak',
+    'America/Anchorage',
+    'America/Araguaina',
+    'America/Argentina/Buenos_Aires',
+    'America/Argentina/Catamarca',
+    'America/Argentina/Cordoba',
+    'America/Argentina/Jujuy',
+    'America/Argentina/La_Rioja',
+    'America/Argentina/Mendoza',
+    'America/Argentina/Rio_Gallegos',
+    'America/Argentina/Salta',
+    'America/Argentina/San_Juan',
+    'America/Argentina/San_Luis',
+    'America/Argentina/Tucuman',
+    'America/Argentina/Ushuaia',
+    'America/Asuncion',
+    'America/Atikokan',
+    'America/Bahia',
+    'America/Bahia_Banderas',
+    'America/Barbados',
+    'America/Belem',
+    'America/Belize',
+    'America/Blanc-Sablon',
+    'America/Boa_Vista',
+    'America/Bogota',
+    'America/Boise',
+    'America/Cambridge_Bay',
+    'America/Campo_Grande',
+    'America/Cancun',
+    'America/Caracas',
+    'America/Cayenne',
+    'America/Chicago',
+    'America/Chihuahua',
+    'America/Costa_Rica',
+    'America/Creston',
+    'America/Cuiaba',
+    'America/Curacao',
+    'America/Danmarkshavn',
+    'America/Dawson',
+    'America/Dawson_Creek',
+    'America/Denver',
+    'America/Detroit',
+    'America/Edmonton',
+    'America/Eirunepe',
+    'America/El_Salvador',
+    'America/Fort_Nelson',
+    'America/Fortaleza',
+    'America/Glace_Bay',
+    'America/Godthab',
+    'America/Goose_Bay',
+    'America/Grand_Turk',
+    'America/Guatemala',
+    'America/Guayaquil',
+    'America/Guyana',
+    'America/Halifax',
+    'America/Havana',
+    'America/Hermosillo',
+    'America/Indiana/Indianapolis',
+    'America/Indiana/Knox',
+    'America/Indiana/Marengo',
+    'America/Indiana/Petersburg',
+    'America/Indiana/Tell_City',
+    'America/Indiana/Vevay',
+    'America/Indiana/Vincennes',
+    'America/Indiana/Winamac',
+    'America/Inuvik',
+    'America/Iqaluit',
+    'America/Jamaica',
+    'America/Juneau',
+    'America/Kentucky/Louisville',
+    'America/Kentucky/Monticello',
+    'America/La_Paz',
+    'America/Lima',
+    'America/Los_Angeles',
+    'America/Maceio',
+    'America/Managua',
+    'America/Manaus',
+    'America/Martinique',
+    'America/Matamoros',
+    'America/Mazatlan',
+    'America/Menominee',
+    'America/Merida',
+    'America/Metlakatla',
+    'America/Mexico_City',
+    'America/Miquelon',
+    'America/Moncton',
+    'America/Monterrey',
+    'America/Montevideo',
+    'America/Nassau',
+    'America/New_York',
+    'America/Nipigon',
+    'America/Nome',
+    'America/Noronha',
+    'America/North_Dakota/Beulah',
+    'America/North_Dakota/Center',
+    'America/North_Dakota/New_Salem',
+    'America/Ojinaga',
+    'America/Panama',
+    'America/Pangnirtung',
+    'America/Paramaribo',
+    'America/Phoenix',
+    'America/Port-au-Prince',
+    'America/Port_of_Spain',
+    'America/Porto_Velho',
+    'America/Puerto_Rico',
+    'America/Punta_Arenas',
+    'America/Rainy_River',
+    'America/Rankin_Inlet',
+    'America/Recife',
+    'America/Regina',
+    'America/Resolute',
+    'America/Rio_Branco',
+    'America/Santarem',
+    'America/Santiago',
+    'America/Santo_Domingo',
+    'America/Sao_Paulo',
+    'America/Scoresbysund',
+    'America/Sitka',
+    'America/St_Johns',
+    'America/Swift_Current',
+    'America/Tegucigalpa',
+    'America/Thule',
+    'America/Thunder_Bay',
+    'America/Tijuana',
+    'America/Toronto',
+    'America/Vancouver',
+    'America/Whitehorse',
+    'America/Winnipeg',
+    'America/Yakutat',
+    'America/Yellowknife',
+    'Antarctica/Casey',
+    'Antarctica/Davis',
+    'Antarctica/DumontDUrville',  
+    'Antarctica/Macquarie',
+    'Antarctica/Mawson',
+    'Antarctica/Palmer',
+    'Antarctica/Rothera',
+    'Antarctica/Syowa',
+    'Antarctica/Troll',
+    'Antarctica/Vostok',
+    'Asia/Almaty',
+    'Asia/Amman',
+    'Asia/Anadyr',
+    'Asia/Aqtau',
+    'Asia/Aqtobe',
+    'Asia/Ashgabat',
+    'Asia/Atyrau',
+    'Asia/Baghdad',
+    'Asia/Baku',
+    'Asia/Bangkok',
+    'Asia/Barnaul',
+    'Asia/Beirut',
+    'Asia/Bishkek',
+    'Asia/Brunei',
+    'Asia/Chita',
+    'Asia/Choibalsan',
+    'Asia/Colombo',
+    'Asia/Damascus',
+    'Asia/Dhaka',
+    'Asia/Dili',
+    'Asia/Dubai',
+    'Asia/Dushanbe',
+    'Asia/Famagusta',
+    'Asia/Gaza',
+    'Asia/Hebron',
+    'Asia/Ho_Chi_Minh',
+    'Asia/Hong_Kong',
+    'Asia/Hovd',
+    'Asia/Irkutsk',
+    'Asia/Jakarta',
+    'Asia/Jayapura',
+    'Asia/Jerusalem',
+    'Asia/Kabul',
+    'Asia/Kamchatka',
+    'Asia/Karachi',
+    'Asia/Kathmandu',
+    'Asia/Khandyga',
+    'Asia/Kolkata',
+    'Asia/Krasnoyarsk',
+    'Asia/Kuala_Lumpur',
+    'Asia/Kuching',
+    'Asia/Macau',
+    'Asia/Magadan',
+    'Asia/Makassar',
+    'Asia/Manila',
+    'Asia/Nicosia',
+    'Asia/Novokuznetsk',
+    'Asia/Novosibirsk',
+    'Asia/Omsk',
+    'Asia/Oral',
+    'Asia/Pontianak',
+    'Asia/Pyongyang',
+    'Asia/Qatar',
+    'Asia/Qostanay',  
+    'Asia/Qyzylorda',
+    'Asia/Riyadh',
+    'Asia/Sakhalin',
+    'Asia/Samarkand',
+    'Asia/Seoul',
+    'Asia/Shanghai',
+    'Asia/Singapore',
+    'Asia/Srednekolymsk',
+    'Asia/Taipei',
+    'Asia/Tashkent',
+    'Asia/Tbilisi',
+    'Asia/Tehran',
+    'Asia/Thimphu',
+    'Asia/Tokyo',
+    'Asia/Tomsk',
+    'Asia/Ulaanbaatar',
+    'Asia/Urumqi',
+    'Asia/Ust-Nera',
+    'Asia/Vladivostok',
+    'Asia/Yakutsk',
+    'Asia/Yangon',
+    'Asia/Yekaterinburg',
+    'Asia/Yerevan',
+    'Atlantic/Azores',
+    'Atlantic/Bermuda',
+    'Atlantic/Canary',
+    'Atlantic/Cape_Verde',
+    'Atlantic/Faroe',
+    'Atlantic/Madeira',
+    'Atlantic/Reykjavik',
+    'Atlantic/South_Georgia',
+    'Atlantic/Stanley',
+    'Australia/Adelaide',
+    'Australia/Brisbane',
+    'Australia/Broken_Hill',
+    'Australia/Currie',
+    'Australia/Darwin',
+    'Australia/Eucla',
+    'Australia/Hobart',
+    'Australia/Lindeman',
+    'Australia/Lord_Howe',
+    'Australia/Melbourne',
+    'Australia/Perth',
+    'Australia/Sydney',
+    'Europe/Amsterdam',
+    'Europe/Andorra',
+    'Europe/Astrakhan',
+    'Europe/Athens',
+    'Europe/Belgrade',
+    'Europe/Berlin',
+    'Europe/Brussels',
+    'Europe/Bucharest',
+    'Europe/Budapest',
+    'Europe/Chisinau',
+    'Europe/Copenhagen',
+    'Europe/Dublin',
+    'Europe/Gibraltar',
+    'Europe/Helsinki',
+    'Europe/Istanbul',
+    'Europe/Kaliningrad',
+    'Europe/Kiev',
+    'Europe/Kirov',
+    'Europe/Lisbon',
+    'Europe/London',
+    'Europe/Luxembourg',
+    'Europe/Madrid',
+    'Europe/Malta',
+    'Europe/Minsk',
+    'Europe/Monaco',
+    'Europe/Moscow',
+    'Europe/Oslo',
+    'Europe/Paris',
+    'Europe/Prague',
+    'Europe/Riga',
+    'Europe/Rome',
+    'Europe/Samara',
+    'Europe/Saratov',
+    'Europe/Simferopol',
+    'Europe/Sofia',
+    'Europe/Stockholm',
+    'Europe/Tallinn',
+    'Europe/Tirane',
+    'Europe/Ulyanovsk',
+    'Europe/Uzhgorod',
+    'Europe/Vienna',
+    'Europe/Vilnius',
+    'Europe/Volgograd',
+    'Europe/Warsaw',
+    'Europe/Zaporozhye',
+    'Europe/Zurich',
+    'Indian/Chagos',
+    'Indian/Christmas',
+    'Indian/Cocos',
+    'Indian/Kerguelen',
+    'Indian/Mahe',
+    'Indian/Maldives',
+    'Indian/Mauritius',
+    'Indian/Reunion',
+    'Pacific/Apia',
+    'Pacific/Auckland',
+    'Pacific/Bougainville',
+    'Pacific/Chatham',
+    'Pacific/Chuuk',
+    'Pacific/Easter',
+    'Pacific/Efate',
+    'Pacific/Enderbury',
+    'Pacific/Fakaofo',
+    'Pacific/Fiji',
+    'Pacific/Funafuti',
+    'Pacific/Galapagos',
+    'Pacific/Gambier',
+    'Pacific/Guadalcanal',
+    'Pacific/Guam',
+    'Pacific/Honolulu',
+    'Pacific/Kiritimati',
+    'Pacific/Kosrae',
+    'Pacific/Kwajalein',
+    'Pacific/Majuro',
+    'Pacific/Marquesas',
+    'Pacific/Nauru',
+    'Pacific/Niue',
+    'Pacific/Norfolk',
+    'Pacific/Noumea',
+    'Pacific/Pago_Pago',
+    'Pacific/Palau',
+    'Pacific/Pitcairn',
+    'Pacific/Pohnpei',
+    'Pacific/Port_Moresby',
+    'Pacific/Rarotonga',
+    'Pacific/Tahiti',
+    'Pacific/Tarawa',
+    'Pacific/Tongatapu',
+    'Pacific/Wake',
+    'Pacific/Wallis',
   ];
 
   const [isFreeEvent, setisFreeEvent] = useState(false);
@@ -57,6 +390,8 @@ function CreateEvent() {
   const [selectedPost, setSelectedPost] = useState(null);
   const [location, setlocation] = useState(false);
   const [eventLink, seteventLink] = useState(false);
+  const [uploadingEvent, setUploadingEvent] = useState(false);
+  const [lockId, setLockId] = useState();
 
   const [error, seterror] = useState(false);
 
@@ -69,11 +404,43 @@ function CreateEvent() {
   };
 
 
+  // Set the minimum end date to the start date
+  const handleStartDateChange = (event) => {
+    setstartDate(event.target.value);
+    setendDate((prevEndDate) => {
+      if (prevEndDate === '' || new Date(prevEndDate) > new Date(event.target.value)) {
+        return prevEndDate;
+      }
+      return event.target.value + 10;
+    });
+  };
+
+
+  // Validate the end date
+  const handleEndDateChange = (event) => {
+    const newEndDate = event.target.value;
+    if (new Date(newEndDate) <= new Date(startDate)) {
+      alert('End date must be later than start date');
+      return;
+    }
+    setendDate(newEndDate);
+  };
+
  
 const abis = require("@unlock-protocol/contracts");
 
 // Wrapping all calls in an async block
 const run = async () => {
+  handleSubmit();
+  console.log(startDate,endDate,timezone,ticketPrice,totalTickets);
+
+   // Set the start and end dates
+const _startDate = new Date(startDate);
+const _endDate = new Date(endDate);
+  const diffInMilliseconds = _endDate - _startDate;
+  const diffInDays = diffInMilliseconds / (1000 * 3600 * 24);
+  console.log(diffInMilliseconds, diffInDays); // Output: 3
+  console.log(diffInDays * 60 * 60 * 24);
   // Here we use a Rinkeby provider. We will be able to read the state, but not send transactions.
   // const provider = new ethers.providers.JsonRpcProvider(
   //   "https://rpc.unlock-protocol.com/5"
@@ -83,6 +450,7 @@ const run = async () => {
   // Note: we sent some fake Eth to this address, but please replace with your own!
    
   //const instance = await State.database.web3Modal.connect();
+  if(!State.database.provider)alert("You need to connect to a web3 wallet to use this feature!") 
   const provider =   State.database.provider;
   console.log("PROVIDER:",provider);
   const signer = provider.getSigner();
@@ -99,13 +467,15 @@ const run = async () => {
   
   // Lock params:
   const lockInterface = new ethers.utils.Interface(abis.PublicLockV11.abi);
+  
+  
   const params = lockInterface.encodeFunctionData(
     "initialize(address,uint256,address,uint256,uint256,string)",
     [
       Address,
-      31 * 60 * 60 * 24, // 30 days in seconds
+      diffInDays * 60 * 60 * 24, // 30 days in seconds
       ethers.constants.AddressZero, // We use the base chain currency
-      ethers.utils.parseUnits(ticketPrice, 18), // 0.01 Eth
+      ethers.utils.parseUnits(ticketPrice.length>0?ticketPrice:"0", 18), // 0.01 Eth
       totalTickets,
       name,
     ]
@@ -116,6 +486,54 @@ const run = async () => {
   const receipt = await transaction.wait();
   const lockAddress = receipt.logs[0].address;
   console.log(lockAddress);
+  setLockId(lockAddress);
+  handleSubmit(lockAddress);
+};
+
+
+const handleSubmit = (lockId) => {
+  //e.preventDefault();
+  setUploadingEvent(true);
+  console.log(name);
+  if (name.length > 1 && lockId) {
+    let data = { 
+      title: name,
+      type: type,
+      category: Category,
+      freeEvent: isFreeEvent,
+      ticketPrice : ticketPrice,
+      unlimitedTickets: isUnlimited,
+      ticketCount : totalTickets,
+      description: description,
+      startTime: startDate,
+      endTime: endDate,
+      timeZone: timezone,
+      eventImage : selectedPost,
+      eventGallery: "",
+      eventHost : State.database.walletAddress,
+      eventUrl : eventLink,
+      location: location,
+      lockId: lockId,   
+    };
+    console.log(data);
+
+    axios
+      .post(`${process.env.REACT_APP_SERVER_URL}/user/addevent`, data, {
+        headers: {
+          "content-type": "application/json",
+          "auth-token": JSON.stringify(localStorage.getItem("authtoken")),
+        },
+      })
+      .then(async (res) => {
+        State.toast("success", "Your event uploaded successfully!");
+        //await clearState();
+      })
+      .catch((err) => {
+        State.toast("error", "Oops!somthing went wrong uploading event!");
+        console.log(err);
+        //clearState();
+      });
+  }
 };
 
   return (
@@ -266,7 +684,7 @@ const run = async () => {
                 </label>
                 <input
                   value={startDate}
-                  onChange={(e) => setstartDate(e.target.value)}
+                  onChange={(e) => handleStartDateChange(e)}
                   className="w-full input"
                   type={"datetime-local"}
                   min={moment().format("YYYY-MM-DDThh:mm")}
@@ -277,15 +695,16 @@ const run = async () => {
                 <label className="ml-2 text-sm font-bold">Event End Time</label>
                 <input
                   value={endDate}
-                  onChange={(e) => setendDate(e.target.value)}
+                  onChange={(e) => handleEndDateChange(e)}
                   className="w-full input"
                   type={"datetime-local"}
-                  min={moment().format("YYYY-MM-DDThh:mm")}
-                  required={true}
+                  min={startDate}
+                  required={true} 
+                   
                 />
               </div>{" "}
               <div className="mt-2 ">
-                <label className="ml-2 text-sm font-bold">Event Category</label>
+                <label className="ml-2 text-sm font-bold">Event Timezone</label>
                 <select
                   onChange={(e) => settimezone(e.target.value)}
                   className="select block w-full font-semibold"
